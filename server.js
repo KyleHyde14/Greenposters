77046ca0f748f9ac81df27288416a154221ddf1b
KyleHyde14 <jesushdezjob@gmail.com>
2023-08-19 20:39:13 +0200

    changing port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
+const PORT = 8080
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -9,6 +10,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-http.listen(5000, () =>{
-    console.log('Server is running at http://localhost:5000')
+http.listen(5000, (y) =>{
+    console.log(`Server is running at https://localhost:${PORT}`)
 })
